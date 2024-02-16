2055ea13917b2d220838594fa60f742502126f6e
Simon Cheek <simon.cheek@yahoo.com>
2024-02-15 21:43:10 -0700

    Finish CustomSwordItem class

--- forge-1.20.2-48.1.0-mdk/src/main/java/net/simoncheek/deadlynightsmod/item/CustomSwordItem.java
+++ forge-1.20.2-48.1.0-mdk/src/main/java/net/simoncheek/deadlynightsmod/item/CustomSwordItem.java
@@ -1,48 +1,15 @@
 package net.simoncheek.deadlynightsmod.item;
 
-
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
-
-import javax.annotation.Nullable;
-import java.awt.*;
 import java.util.Random;
-import java.util.logging.Level;
 
 
 public class CustomSwordItem extends SwordItem {
 
-    float modifiedDamage = calculateModifiedDamage();
-    boolean modified = false;
-
     public CustomSwordItem(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
         super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
-    }
-
-
-    private float calculateModifiedDamage(){
-        float baseDamage = this.getDamage();
-        Random creationRand = new Random();
-        int creationInt = creationRand.nextInt(100);
-        if (creationInt <= 50) {
-            baseDamage += 100;
-            this.modified = true;
-        }
-        return baseDamage;
-    }
-
-
-    @Override
-    public void appendHoverText(ItemStack pStack, @org.jetbrains.annotations.Nullable net.minecraft.world.level.Level pLevel, java.util.List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
-        if (this.modified) {
-            pTooltipComponents.add(Component.literal("Modified!"));
-        }
-        pTooltipComponents.add(Component.literal("Basic"));
-        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 
 
@@ -56,7 +23,7 @@ public class CustomSwordItem extends SwordItem {
         Random rand = new Random();
         int rand_chance = rand.nextInt(100);
         if (rand_chance <= 10) {
-            baseDamage *= 1.8f;
+            baseDamage *= 1.5f;
         }
 
         target.hurt(attacker.damageSources().playerAttack((Player) attacker), baseDamage);
