2aa6b0593c244c35824d74328b8e835d9bcf2d62
Simon Cheek <simon.cheek@yahoo.com>
2024-02-16 14:26:20 -0700

    Add Event Package

---
+++ forge-1.20.2-48.1.0-mdk/src/main/java/net/simoncheek/deadlynightsmod/event/ModEvents.java
@@ -0,0 +1,9 @@
+package net.simoncheek.deadlynightsmod.event;
+
+
+import net.minecraftforge.fml.common.Mod;
+import net.simoncheek.deadlynightsmod.DeadlyNightsMod;
+
+@Mod.EventBusSubscriber(modid = DeadlyNightsMod.MODID)
+public class ModEvents {
+}
