acc7b2814cf2de3bad4c0df9506dea79fc77eb49
Simon Cheek <simon.cheek@yahoo.com>
2024-02-16 20:05:51 -0700

    create events package

--- forge-1.20.2-48.1.0-mdk/src/main/java/net/simoncheek/deadlynightsmod/event/ModEvents.java
+++ forge-1.20.2-48.1.0-mdk/src/main/java/net/simoncheek/deadlynightsmod/event/ModEvents.java
@@ -1,9 +1,17 @@
 package net.simoncheek.deadlynightsmod.event;
 
 
+import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.living.MobSpawnEvent;
+import net.minecraftforge.event.entity.living.ZombieEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.simoncheek.deadlynightsmod.DeadlyNightsMod;
 
+import java.util.Random;
+
 @Mod.EventBusSubscriber(modid = DeadlyNightsMod.MODID)
 public class ModEvents {
+
 }
